cab74226051b5977f65bd91debde14a56590d2f7
Tonnika <44046572+Tonnika@users.noreply.github.com>
2018-11-19 20:42:10 -0800

    update

--- world.php
+++ world.php
@@ -1,4 +1,6 @@
 <?php
+$country =$_GET['country'];
+$all = $_GET['all'];
 
 $host = getenv('IP');
 $username = getenv('C9_USER');
@@ -6,13 +8,21 @@ $password = '';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-
-$stmt = $conn->query("SELECT * FROM countries");
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo '<ul>';
-foreach ($results as $row) {
-  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+if($all == "true"){
+    $pstring =$conn->query( "SELECT * FROM countries");
+    $results = $pstring->fetchAll(PDO::FETCH_ASSOC);
+    echo '<ul>';
+    foreach ($results as $row) {
+         echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
 }
-echo '</ul>';
+else{
+    $pstring ="SELECT * FROM countries WHERE name LIKE '%".$country."%'";
+    $results = $pstring->fetchAll(PDO::FETCH_ASSOC);
+    echo '<ul>';
+    foreach ($results as $row) {
+        if($row['name']==$country){
+         echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+}
+}
